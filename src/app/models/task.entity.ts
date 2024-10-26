1d0e51a9c31c797cfb0775ac3dca7b0743b451d1
anderson <andsantos979@gmail.com>
2024-10-26 12:20:16 -0600

    Mejoras en lo visual y en opciones del select

--- src/app/models/task.entity.ts
+++ src/app/models/task.entity.ts
@@ -2,9 +2,9 @@ export class TaskEntity{
     id: number; 
     user_name: string; 
     task_description: string; 
-    task_state: 'Pendiente' | 'en progreso' | 'completado';
+    task_state: 'Pendiente' | 'En progreso' | 'Completado';
 
-    constructor(id: number, user_name: string, task_description: string, task_state: 'Pendiente' | 'en progreso' | 'completado') {
+    constructor(id: number, user_name: string, task_description: string, task_state: 'Pendiente' | 'En progreso' | 'Completado') {
         this.id = id;
         this.user_name = user_name;
         this.task_description = task_description;
