94d682b6f0dcc6645b8d3bb3373250ad7a222cae
anderson <andsantos979@gmail.com>
2024-10-25 19:58:46 -0600

    Correcciones

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,14 +15,33 @@ export class LoginComponent{
     submit(form: any){
         this.apiService.login(form.username, form.password).subscribe({
             next: (response) => {
-                localStorage.setItem('access_token',response.access_token);
+                // localStorage.setItem('access_token',response.access_token);
 
-                const decodToken: any = jwtDecode(response.access_token);
-                console.log(decodToken)
-                const rol = decodToken.rol;
-                localStorage.setItem('user_rol',rol);
-                console.log(rol)
-                this.router.navigate(['/panel']);
+                // const decodToken: any = jwtDecode(response.access_token);
+                // console.log(decodToken)
+                // const rol = decodToken.rol;
+                // localStorage.setItem('user_rol',rol);
+                // console.log(rol)
+                // this.router.navigate(['/panel']);
+                const token = response.access_token;
+                if (token) {
+                    localStorage.setItem('access_token', token);
+    
+                    try {
+                        const decodedToken: any = jwtDecode(token);
+                        console.log(decodedToken);
+    
+                        const rol = decodedToken.rol;
+                        localStorage.setItem('user_rol', rol);
+                        console.log(rol);
+    
+                        this.router.navigate(['/panel']);
+                    } catch (error) {
+                        console.error('Error al decodificar el token:', error);
+                    }
+                } else {
+                    console.error('No se recibió un token válido');
+                }
             },
             error: (err)=>{
                 console.error('Algo fallo: ',err);
